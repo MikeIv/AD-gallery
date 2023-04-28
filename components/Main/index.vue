a3068ba3c58fefd9b6b662c90dd25cbc6049259e
MIUIvanov <MIUIvanov@synergy.ru>
2023-04-28 20:56:15 +0300

    Трансформация изображения

--- components/Main/index.vue
+++ components/Main/index.vue
@@ -12,7 +12,7 @@
     <div class="main__gallery">
       <ul class="main__gallery-list" :class="{ next: setGallery }">
         <li
-          v-show="!setGallery"
+          v-if="!setGallery"
           class="main__gallery-item"
           :class="`item-${item.id}`"
           v-for="item in imgList"
@@ -21,7 +21,7 @@
           <img :src="item.links" />
         </li>
         <li
-          v-show="!!setGallery"
+          v-if="!!setGallery"
           class="main__gallery-item"
           :class="`item-g${index}`"
           v-for="(item, index) in imgListNext"
@@ -31,41 +31,6 @@
         </li>
       </ul>
     </div>
-
-    <!--    <div class="main__block">-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02568.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02578.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02602.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02671.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02674.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02715.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02759.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02838.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02907.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02923.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH02948.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03008.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03020.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03048.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03061.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03096.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03170.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03186.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03211.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03271.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03306.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03451.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03471.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03559.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03567.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03579.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03707.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03754.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03777.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03813.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH03910.jpg" />-->
-    <!--      <img src="https://cdn.ascgroup.ru/news_media/13099/ACH04011.jpg" />-->
-    <!--    </div>-->
   </section>
 </template>
 
@@ -79,10 +44,10 @@ import AButton from '~/components/_ui/AButton/a_button.vue';
 const setGallery = ref(false);
 
 const toggleGalleryFirst = () => {
-  setGallery.value ? (setGallery.value = false) : false;
+  setGallery.value ? (setGallery.value = false) : '';
 };
 const toggleGalleryNext = () => {
-  !setGallery.value ? (setGallery.value = true) : true;
+  !setGallery.value ? (setGallery.value = true) : '';
 };
 
 const { imgLinks } = imgData();
