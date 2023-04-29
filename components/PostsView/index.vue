ad3ba3cfea202bd019bae1349419c5931846c633
MIUIvanov <MIUIvanov@synergy.ru>
2023-04-29 14:29:09 +0300

    feat(custom): страница поста
    Страница поста

--- components/PostsView/index.vue
+++ components/PostsView/index.vue
@@ -1,10 +1,24 @@
 <template>
-  <section class="posts-view">
-    Статья
+  <section class="posts-view layout-custom__wrapper">
+    <h2>{{ $route.params.id }}</h2>
+    <div class="posts-view__img-wrapper">
+      <img :src="imgList[0].links" alt="image" class="posts-view__img" />
+    </div>
+    <NuxtLink to="/" class="main__gallery-link">
+      <span class="a-font__s">На главную</span>
+    </NuxtLink>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+const route = useRoute();
+const imgId = route.params.id;
+console.log('imgId', imgId);
+
+const { imgLinks } = imgData();
+const imgList = imgLinks.value.filter((value) => value.id === imgId);
+console.log('imgList', imgList);
+</script>
 
 <style lang="scss">
 @import './posts-view.scss';
--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout-custom bg-blue-500 text-white">
+  <div class="layout-custom">
     <slot />
   </div>
 </template>
@@ -10,4 +10,48 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.layout-custom {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+  min-height: 100vh;
+  padding-bottom: rem(40);
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    max-width: 100%;
+    min-height: 100vh;
+    margin: 0 auto;
+    padding: rem(80) rem(12) rem(20) rem(12);
+
+    @media screen and (min-width: $tablet) {
+      max-width: rem(768);
+      padding: rem(80) rem(40);
+    }
+    @media screen and (min-width: $tabletMax) {
+      max-width: rem(832);
+    }
+    @media screen and (min-width: $desktopMin) {
+      max-width: rem(1080);
+    }
+  }
+
+  .nuxt-icon {
+    display: flex;
+    width: 100%;
+    height: 100%;
+
+    svg {
+      object-fit: cover;
+      width: auto;
+      height: 100%;
+      margin: 0;
+    }
+  }
+}
+</style>
